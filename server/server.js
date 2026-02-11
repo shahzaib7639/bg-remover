7e2cdeca114bc99e9af872453cd5f10a9cd874a3
Muhammad Shahzaib <muhammadshahzaib7639@gmail.com>
2026-02-11 19:36:09 +0500

    First commit

--- server/server.js
+++ server/server.js
@@ -2,17 +2,15 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
-
-//App Config
+// App Config
 const PORT = process.env.PORT || 4000;
 const app = express();
 await connectDB();
 
-//Intialize Middlewares
+//Intialize Middleware
 app.use(express.json());
 app.use(cors());
-
-// API routes
+// API Routes
 app.get("/", (req, res) => res.send("API Working"));
 
-app.listen(PORT, () => console.log("server Running on PORT " + PORT));
+app.listen(PORT, () => console.log("Server Running on port" + PORT));
